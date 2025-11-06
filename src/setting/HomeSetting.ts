413311be905e7f4e0ba9dbb6b7ed59ab10df4a54
XinCheng <107662142+XinChengP@users.noreply.github.com>
2025-11-06 20:47:20 +0800

    Update HomeSetting.ts

--- src/setting/HomeSetting.ts
+++ src/setting/HomeSetting.ts
@@ -1,13 +1,14 @@
 //主标题
-export const mainTitle = "0xygen Blog";
+export const mainTitle = "歆橙 的 Blog";
 //主题色装饰字
-export const mainTitleBlueDecoration = ".";
+export const mainTitleBlueDecoration = "awa";
 //副标题
-export const subTitle = "A Front-End ";
+export const subTitle = "一个普普通通的 ";
 //副标题主题色装饰字
-export const subTitleBlueDecoration = "Developer";
+export const subTitleBlueDecoration = "锦依卫";
 //打字机动画轮播数组
 export const TypewriterTexts = [
-  "悟以往之不谏，知来者之可追",
-  "JavaScript是最好的语言!",
+  "佬啊，佬啊，你怎么只是纸片人啊",
+  "天行健，君子以自强不息",
+  "学习是件痛苦又有趣的事",
 ];
